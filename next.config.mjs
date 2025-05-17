149e9ca1caf980b4d5e071131b53bb3018fa83a6
devakapatel <dev.akash.patel@gmail.com>
2025-05-17 11:43:56 +0530

    final I guess

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export', // Add this here
 };
 
 export default withMDX(config);
-
-const nextConfig = {
-  output: 'export'
-}
